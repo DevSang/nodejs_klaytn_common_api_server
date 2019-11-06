ed0addd673a50988cccd4693b81a8ea1a66ca9f6
haeri <haeri@looncup.com>
2019-11-06 14:03:04 +0900

    getMonth 수정

--- src/api/token/tokenAPI.js
+++ src/api/token/tokenAPI.js
@@ -109,7 +109,7 @@ exports.sendToken = async (req, res, next) => {
         if(!receiverUser || receiverUser.length == 0) return res.status(401).json({message: `Recever User not found(adderss: ${address})`});
         receiverInfo = receiverUser[0];
       }else {
-        const gemHistory = await prisma.gemTransactions({where: {rewardType: contents, receiverUserRowId: res.locals.user.id, status: true, createTime_gte: new Date(date)}, orderBy: 'createTime_DESC'});
+        const gemHistory = await prisma.gemTransactions({where: {rewardType: contents, receiverUserRowId: res.locals.user.id, status: true, createTime_gte: new Date(`${today.getFullYear()}-${today.getMonth() +1 }-01`)}, orderBy: 'createTime_DESC'});
         if(gemHistory.length > 0) {
           const check = await gemHistory.some((old) => {
               paidGem += old.amount;
